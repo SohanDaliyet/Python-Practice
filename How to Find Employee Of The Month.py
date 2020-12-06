67fb227695ce7f27ea061a82ae16b43ad2baf0e2
Sohan Daliyet <64396845+SohanDaliyet@users.noreply.github.com>
2020-12-06 18:10:55 +0530

    Made A Function

--- How to Find Employee Of The Month.py
+++ How to Find Employee Of The Month.py
@@ -1,1 +1,6 @@
 work_hours = [('Abby',100),('Billy',400),('katty',800)]
+#These are predefined values in the terms of (Employee,Number of hours worked)
+def employee_check(work_hours):
+
+    current_max = 0
+    employee_of_the_month = ''
