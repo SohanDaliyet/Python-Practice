f504425b6125d823e79bc9dbb043a2ce828d0df6
Sohan Daliyet <64396845+SohanDaliyet@users.noreply.github.com>
2020-12-06 18:12:29 +0530

    Completed the employee_check Function

--- How to Find Employee Of The Month.py
+++ How to Find Employee Of The Month.py
@@ -4,3 +4,12 @@ def employee_check(work_hours):
 
     current_max = 0
     employee_of_the_month = ''
+    for employee,hours in work_hours:
+        if hours > current_max:
+            current_max = hours
+            employee_of_the_month = employee
+        else:
+            pass
+
+    return(employee_of_the_month,current_max)
+
