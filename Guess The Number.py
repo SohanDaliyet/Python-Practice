f3c6d6113697794284eeedecf5d08ce02321f8c3
Sohan Daliyet <64396845+SohanDaliyet@users.noreply.github.com>
2020-12-07 11:12:33 +0530

    Created Function 'player_guess'

--- Guess The Number.py
+++ Guess The Number.py
@@ -3,3 +3,11 @@ from random import shuffle
 def shuffle_list(my_list):
     shuffle(my_list)
     return my_list
+
+def player_guess():
+
+    guess = ''
+
+    while guess not in ['0','1','2']:
+        guess = input('''Pick A Number ['0';'1';'2'] :''')
+
