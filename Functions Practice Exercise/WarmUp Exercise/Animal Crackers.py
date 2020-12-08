5fd84c51a9f5ecdd7529240f673409f18ebeb3b7
Sohan Daliyet <64396845+SohanDaliyet@users.noreply.github.com>
2020-12-08 12:49:06 +0530

    Update Animal Crackers.py

--- Functions Practice Exercise/WarmUp Exercise/Animal Crackers.py
+++ Functions Practice Exercise/WarmUp Exercise/Animal Crackers.py
@@ -1,3 +1,5 @@
+#This is the given question.
+
 #ANIMAL CRACKERS: Write a function takes a two-word string and returns True if both words begin with same letter
 #animal_crackers('Levelheaded Llama') --> True
 #animal_crackers('Crazy Kangaroo') --> False
