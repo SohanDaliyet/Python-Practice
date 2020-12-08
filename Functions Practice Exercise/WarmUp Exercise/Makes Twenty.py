0f2f391f884989690687028d018fc8917f2baca4
Sohan Daliyet <64396845+SohanDaliyet@users.noreply.github.com>
2020-12-08 12:49:53 +0530

    Update Makes Twenty.py

--- Functions Practice Exercise/WarmUp Exercise/Makes Twenty.py
+++ Functions Practice Exercise/WarmUp Exercise/Makes Twenty.py
@@ -1,3 +1,5 @@
+#This is the given question.
+
 #MAKES TWENTY: Given two integers, return True if the sum of the integers is 20 or if one of the integers is 20. If not, return False
 #makes_twenty(20,10) --> True
 #makes_twenty(12,8) --> True
