c1f1e1b400c5419b1b02577d2d03244f114677da
Sohan Daliyet <64396845+SohanDaliyet@users.noreply.github.com>
2020-12-08 12:52:22 +0530

    Update PAPER DOLL.py

--- Functions Practice Exercise/Level - 2 Problems/PAPER DOLL.py
+++ Functions Practice Exercise/Level - 2 Problems/PAPER DOLL.py
@@ -1,3 +1,5 @@
+#This is the given question.
+
 #PAPER DOLL: Given a string, return a string where for every character in the original there are three characters
 #paper_doll('Hello') --> 'HHHeeellllllooo'
 #paper_doll('Mississippi') --> 'MMMiiissssssiiippppppiii'
