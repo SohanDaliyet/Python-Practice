2a1b354ac15251dc503eee88d05d493aa653896c
Sohan Daliyet <64396845+SohanDaliyet@users.noreply.github.com>
2020-12-10 22:08:10 +0530

    Declared Function 'animal_cracker'

--- Functions Practice Exercise/WarmUp Exercise/Animal Crackers.py
+++ Functions Practice Exercise/WarmUp Exercise/Animal Crackers.py
@@ -3,3 +3,8 @@
 #ANIMAL CRACKERS: Write a function takes a two-word string and returns True if both words begin with same letter
 #animal_crackers('Levelheaded Llama') --> True
 #animal_crackers('Crazy Kangaroo') --> False
+
+def animal_cracker(text):
+    wordlist = text.upper().split()
+    #print(wordlist)
+    return wordlist[0][0] == wordlist[1][0]
