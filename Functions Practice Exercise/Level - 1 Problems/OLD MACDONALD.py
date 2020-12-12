38c6b938570a179dba40a4edcd06ff63ebbeecb6
Sohan Daliyet <64396845+SohanDaliyet@users.noreply.github.com>
2020-12-12 09:55:47 +0530

    Declared Function 'old_macdonald'

--- Functions Practice Exercise/Level - 1 Problems/OLD MACDONALD.py
+++ Functions Practice Exercise/Level - 1 Problems/OLD MACDONALD.py
@@ -4,3 +4,10 @@
 #old_macdonald('macdonald') --> MacDonald
 
 #Note: 'macdonald'.capitalize() returns 'Macdonald'
+
+def old_macdonald(name):
+
+    first_half = name[:3]
+    second_half = name[3:]
+
+    return first_half.capitalize() + second_half.capitalize()
